897a7ed32f582872a3afe49b337626cb3210d7ec
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-07-17 11:06:03 +0900

    test3

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,5 @@
-"use client";
-import { useEffect, useState } from "react";
+import ClientGreeting from "./ClientGreeting";
 
-type Lang = "kr" | "en" | "jp";
-
-export default function Home() {
-  const [lang, setLang] = useState<Lang>("kr");
-  const [hello, setHello] = useState<string | undefined>();
-
-  useEffect(() => {
-    hi();
-  }, []);
-
-  useEffect(() => {
-    getGreet();
-  }, [lang]);
-
-  async function getGreet() {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/greeting?lang=${lang}`
-    );
-    if (!res.ok) throw new Error(res.statusText);
-    setHello(await res.text());
-  }
-
-  async function hi() {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`);
-    if (!res.ok) throw new Error(res.statusText);
-  }
-
-  return (
-    <div>
-      <h1>현재 선택된 언어: {lang}</h1>
-
-      <select
-        name="lang"
-        id="lang"
-        onChange={(e) => setLang(e.target.value as Lang)}
-        value={lang}
-      >
-        <option value="kr">한국어</option>
-        <option value="en">English</option>
-        <option value="jp">日本語</option>
-      </select>
-      {hello !== undefined ? <div>{hello}</div> : <div>loading...</div>}
-    </div>
-  );
+export default function Page() {
+  return <ClientGreeting />;
 }
